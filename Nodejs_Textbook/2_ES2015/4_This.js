84d6b96c511389d0f1aaa69c8b3125c7cfa9da67
DotNetTutorial <85148810+roy1117@users.noreply.github.com>
2025-06-30 00:44:38 +0900

    [this][]Array deconstruction

--- Nodejs_Textbook/2_ES2015/4_This.js
+++ Nodejs_Textbook/2_ES2015/4_This.js
@@ -4,7 +4,44 @@ var relationship1 = {
     logfriends: function(){
         var that = this;
         this.friend.forEach(function (friend){
-            console.log(that.name, friend);
+            console.log(this.name, friend);
         });
     },
 };
+
+var relationship2 = {
+    name: 'zero',
+    friend : ['nero', 'hero', 'xero'],
+    logfriends(){
+        this.friend.forEach((friend) =>{
+            console.log(this.name, friend);
+        });
+    },
+};
+
+var scopeTest = {
+    name: 'Some misterious name',
+    age : 33,
+    projects : ['project A', 'project B', 'project C'],
+    display() {
+        console.log(this.name);
+        console.log(this.age);
+        var that = this;
+        this.projects.forEach(function(project){
+            console.log(project, that.name);
+        })
+    }
+}
+
+testFunction = function(){
+    var list = ['fruit', 'brick'];
+    list.forEach(function (item){
+        console.log(item);
+    })
+}
+
+scopeTest.display();
+testFunction();
+
+relationship1.logfriends();
+relationship2.logfriends();
---
+++ Nodejs_Textbook/2_ES2015/5_Array_Destructuring.js
@@ -0,0 +1,16 @@
+var candyMachine = {
+    status : {
+        name: 'node',
+        count: 5
+    },
+    getCandy: function(){
+        this.status.count--;
+        return this.status.count;
+    },
+};
+
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+
+console.log(candyMachine.getCandy());
+console.log(candyMachine.getCandy());
